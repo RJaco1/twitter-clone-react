399b59e0dfe5441061bf4ed6725ff6ce03812a9c
salvadorperez01 <salvador.perez01@telusinternational.com>
2021-11-20 22:30:15 -0600

    Added follow feature locally

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,37 +1,47 @@
-import React, { forwardRef, useState, useEffect } from "react";
-import { Avatar } from "@material-ui/core";
-import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
-import RepeatIcon from "@material-ui/icons/Repeat";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import PublishIcon from "@material-ui/icons/Publish";
-import { Modal, Button, Row } from "react-bootstrap";
+import React, { forwardRef, useState, useEffect } from 'react';
+import { Avatar } from '@material-ui/core';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
+import { Modal, Button, Row } from 'react-bootstrap';
 
-import "../../css/post.css";
-import db from "../../services/firebase";
-import { doc, updateDoc } from "firebase/firestore";
+import '../../css/post.css';
+import db from '../../services/firebase';
+import { doc, updateDoc } from 'firebase/firestore';
 
 const Post = forwardRef(
   (
     { displayName, username, verified, text, image, avatar, like, postId },
     ref
   ) => {
-    const [color, setColor] = useState("");
+    const [color, setColor] = useState('');
     const [show, setShow] = useState(false);
+    const [followStyle, setFollowStyle] = useState(false);
+    const [follow, setFollow] = useState('Follow');
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     useEffect(() => {
-      like ? setColor("red") : setColor("");
+      like ? setColor('red') : setColor('');
     }, [like]);
 
     const updateLike = async () => {
       like ? (like = false) : (like = true);
-      const postRef = doc(db, "posts", postId);
+      const postRef = doc(db, 'posts', postId);
       await updateDoc(postRef, {
         like: like,
       });
+    };
+
+    const updateFollow = () => {
+      if (follow === 'Follow') {
+        setFollow('Unfollow');
+      } else {
+        setFollow('Follow');
+      }
     };
 
     return (
@@ -43,15 +53,19 @@ const Post = forwardRef(
           <div className="post__header">
             <div className="post__headerText">
               <h3>
-                {displayName}{" "}
+                {displayName}{' '}
                 <span className="post__headerSpecial">
                   {verified && <VerifiedUserIcon className="post__badge" />} @
                   {username}
-                  <Button type="submit" className="tweetBox__tweetButton">
-                    follow
-                  </Button>
                 </span>
               </h3>
+              <Button
+                type="submit"
+                className="tweetBox__followButton"
+                onClick={updateFollow}
+              >
+                {follow}
+              </Button>
             </div>
             <div className="post__headerDescription">
               <p>{text}</p>
